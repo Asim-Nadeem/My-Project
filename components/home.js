0c4f937b9c105fa3dc4ce69422de3b1eaf7a2bd6
Asim-Nadeem <17033105@stu.mmu.ac.uk>
2021-01-29 17:43:04 +0000

    Login and Sign up with Navigation

--- components/home.js
+++ components/home.js
@@ -4,16 +4,19 @@ import { StyleSheet, View, Text } from 'react-native'
 const home = () => {
   return (
     <View style={styles.MainView}>
+      <View style={{ backgroundColor: '#FF7077', height: '10%', width: '100%', justifyConetent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 50 }}>Coffee Review</Text>
+      </View>
       <Text>First Tab in React Native</Text>
     </View>
   )
 }
 const styles = StyleSheet.create({
   MainView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'orange'
+    flex: 1
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    // backgroundColor: 'orange'
 
   }
 })
